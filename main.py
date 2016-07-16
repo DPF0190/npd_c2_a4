9d53a0c5a8f9ae261f94498428ed1fffd5e1573e
DPF0190 <domenick.fumai@gmail.com>
2016-07-16 21:08:06 +0000

    adding files

---
+++ main.py
@@ -0,0 +1,27 @@
+import json
+import yaml
+import csv
+from pprint import pprint
+
+
+FILENAME_JSON = 'input.json'
+FILENAME_CSV = 'input.csv'
+FILENAME_YAML = 'input.yaml'
+
+with open(FILENAME_JSON, 'r') as json_file:
+    json_data = json.load(json_file)
+pprint(json_data)
+
+with open(FILENAME_YAML, 'r') as yaml_file:
+    yaml_data = yaml.load(yaml_file)
+pprint(yaml_file)
+
+with open(FILENAME_CSV, 'r') as csv_file:
+    reader = csv.reader(FILENAME_CSV)
+    names = next(reader)
+    lines = [l for l in reader]
+print(names)
+print(lines)
+
+
+
