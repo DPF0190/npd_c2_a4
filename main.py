fb1ec1bd09fcbb36f44b070e54d69798ff332a36
DPF0190 <domenick.fumai@gmail.com>
2016-07-19 00:25:05 +0000

    updated

--- main.py
+++ main.py
@@ -4,33 +4,41 @@ import csv
 from pprint import pprint
 
 
-FILENAME_JSON = 'input.json'
+# FILENAME_JSON = 'input.json'
 FILENAME_CSV = 'input.csv'
 FILENAME_YAML = 'input.yaml'
 
-def json_loader(filepath):
-    with open(FILENAME_JSON, 'r') as json_file:
-        json_data = json.load(json_file)
+# with open(FILENAME_JSON, 'r') as json_file:
+#         json_data = json.load(json_file)
 
-def yaml_loader(filepath):
-    with open(FILENAME_YAML, 'r') as yaml_file:
+with open(FILENAME_YAML, 'r') as yaml_file:
         yaml_data = yaml.load(yaml_file)
 
-def csv_loader(filepath):
-    with open(FILENAME_CSV, 'r') as csv_file:
-        reader = csv.reader(FILENAME_CSV)
+yaml_list_dicts = yaml_data['data']
+
+with open(FILENAME_CSV, 'r') as csv_file:
+        reader = csv.reader(csv_file)
         names = next(reader)
         lines = [l for l in reader]
 
-if __name__ == "__main__":
-    yaml_file = "input.yaml"
-    yaml_data = yaml.load(yaml_file)
-    return (yaml_data)
+csv_list_dicts = [
+    {names[col_num]: col_val for col_num, col_val in enumerate(line)}
+    for line in lines]
+
+combined_list_of_dicts = yaml_list_dicts + csv_list_dicts
 
 
+with open('output.csv', 'w') as csv_out:
+    header = ','.join(names)
+    csv_out.write(header + '\n')
+    for dict_val in combined_list_of_dicts:
+        row_list = [str(dict_val[name]) for name in names]
+        row_string = ','.join(row_list)
+        csv_out.write(row_string + '\n')
 
 
-
+with open('output.json', 'w') as json_out:
+    json.dump(combined_list_of_dicts, json_out)
 
 
 
